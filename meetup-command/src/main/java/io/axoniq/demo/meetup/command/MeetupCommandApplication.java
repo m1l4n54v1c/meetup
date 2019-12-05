01e00163a8988f711ddd86ab4f9f009b54c2e574
Milan Savic <milan.savic@axoniq.io>
2019-12-05 15:29:52 +0100

    Replaced AxonServerCommandBus with SimpleCommandBus.

--- meetup-command/src/main/java/io/axoniq/demo/meetup/command/MeetupCommandApplication.java
+++ meetup-command/src/main/java/io/axoniq/demo/meetup/command/MeetupCommandApplication.java
@@ -1,7 +1,11 @@
 package io.axoniq.demo.meetup.command;
 
 import org.axonframework.axonserver.connector.ErrorCode;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.ErrorContext;
+import org.axonframework.eventhandling.ErrorHandler;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.EventMessageHandler;
 import org.axonframework.eventhandling.ListenerInvocationErrorHandler;
@@ -10,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Milan Savic
@@ -25,14 +30,33 @@ public class MeetupCommandApplication {
 
     @Autowired
     public void configure(EventProcessingConfigurer eventProcessingConfigurer) {
-        eventProcessingConfigurer.registerListenerInvocationErrorHandler("some-group", configuration -> new ListenerInvocationErrorHandler() {
-            @Override
-            public void onError(Exception e, EventMessage<?> eventMessage, EventMessageHandler eventMessageHandler)
-                    throws Exception {
-                if(e.getMessage().contains(ErrorCode.INVALID_EVENT_SEQUENCE.errorCode())) {
-                    log.error("I got u!");
-                }
+        InvalidSequenceErrorHandler iseh = new InvalidSequenceErrorHandler();
+        eventProcessingConfigurer.registerListenerInvocationErrorHandler("some-group", configuration -> iseh);
+        eventProcessingConfigurer.registerErrorHandler("some-group", configuration -> iseh);
+    }
+
+    @Bean
+    public CommandBus commandBus() {
+        return SimpleCommandBus.builder().build();
+    }
+
+    private static class InvalidSequenceErrorHandler implements ListenerInvocationErrorHandler, ErrorHandler {
+
+        @Override
+        public void handleError(ErrorContext errorContext) throws Exception {
+            handle(errorContext.error());
+        }
+
+        @Override
+        public void onError(Exception exception, EventMessage<?> event, EventMessageHandler eventHandler)
+                throws Exception {
+            handle(exception);
+        }
+
+        private void handle(Throwable e) {
+            if (e.getMessage().contains(ErrorCode.INVALID_EVENT_SEQUENCE.errorCode())) {
+                log.error("I got u!");
             }
-        });
+        }
     }
 }
